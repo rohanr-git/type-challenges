74cedb314209b919053b2ae081ad8cdcbb668001
Apurupareddy <93178414+Apurupareddy@users.noreply.github.com>
2023-12-08 19:00:16 -0800

    Update loader.ts

--- scripts/loader.ts
+++ scripts/loader.ts
@@ -1,117 +1,167 @@
-import path from 'path'
-import fs from 'fs-extra'
-import fg from 'fast-glob'
-import YAML from 'js-yaml'
-import type { Quiz, QuizMetaInfo } from './types'
-import { defaultLocale, supportedLocales } from './locales'
+import path from 'path';
+import fs from 'fs-extra';
+import fg from 'fast-glob';
+import YAML from 'js-yaml';
+import type { Quiz, QuizMetaInfo } from './types';
+import { defaultLocale, supportedLocales } from './locales';
 
-export async function loadFile(filepath: string) {
-  if (fs.existsSync(filepath))
-    return await fs.readFile(filepath, 'utf-8')
-  return undefined
-}
+// Encapsulated module
+const QuizLoader = (function () {
+  // Private variables
+  const QUIZ_ROOT = path.resolve(__dirname, '../questions');
 
-export async function loadLocaleVariations<T = string>(
-  filepath: string,
-  preprocessor: (s: string) => T = s => s as any as T,
-) {
-  const { ext, dir, name } = path.parse(filepath)
-  const data: Record<string, T> = {}
-
-  for (const locale of supportedLocales) {
-    const file = preprocessor(await loadFile(path.join(dir, `${name}.${locale}${ext}`)) || '')
-
-    if (file)
-      data[locale] = file
-  }
-
-  if (!data[defaultLocale]) {
-    // default version
-    const file = preprocessor(await loadFile(filepath) || '')
-    if (file)
-      data[defaultLocale] = file
-  }
-
-  return data
-}
-
-export function readmeCleanUp(text: string) {
-  return text
-    .replace(/<!--info-header-start-->[\s\S]*<!--info-header-end-->/, '')
-    .replace(/<!--info-footer-start-->[\s\S]*<!--info-footer-end-->/, '')
-    .trim()
-}
-
-export function loadInfo(s: string): Partial<QuizMetaInfo> | undefined {
-  const object = YAML.load(s) as any
-  if (!object)
-    return undefined
-
-  const arrayKeys = ['tags', 'related']
-
-  for (const key of arrayKeys) {
-    if (object[key]) {
-      object[key] = (object[key] || '')
-        .toString()
-        .split(',')
-        .map((i: string) => i.trim())
-        .filter(Boolean)
-    }
-    else {
-      object[key] = undefined
+  // Private function to load a file
+  async function loadFile(filepath: string): Promise<string | undefined> {
+    try {
+      if (await fs.pathExists(filepath)) {
+        return await fs.readFile(filepath, 'utf-8');
+      }
+      return undefined;
+    } catch (error) {
+      console.error(`Error while loading file: ${error.message}`);
+      return undefined;
     }
   }
 
-  return object
-}
+  // Private function to load variations for different locales
+  async function loadLocaleVariations<T = string>(
+    filepath: string,
+    preprocessor: (s: string) => T = (s) => s as any as T,
+  ): Promise<Record<string, T>> {
+    const { ext, dir, name } = path.parse(filepath);
+    const data: Record<string, T> = {};
 
-export const QUIZ_ROOT = path.resolve(__dirname, '../questions')
+    for (const locale of supportedLocales) {
+      const file = preprocessor(await loadFile(path.join(dir, `${name}.${locale}${ext}`)) || '');
 
-export async function loadQuizes(): Promise<Quiz[]> {
-  const folders = await fg('{0..9}*-*', {
-    onlyDirectories: true,
-    cwd: QUIZ_ROOT,
-  })
+      if (file) {
+        data[locale] = file;
+      }
+    }
 
-  const quizes = await Promise.all(
-    folders.map(async dir => loadQuiz(dir)),
-  )
+    if (!data[defaultLocale]) {
+      // Default version
+      const file = preprocessor(await loadFile(filepath) || '');
+      if (file) {
+        data[defaultLocale] = file;
+      }
+    }
 
-  return quizes
-}
+    return data;
+  }
 
-export async function loadQuiz(dir: string): Promise<Quiz> {
+  // Private function to clean up readme text
+  function readmeCleanUp(text: string): string {
+    return text
+      .replace(/<!--info-header-start-->[\s\S]*<!--info-header-end-->/, '')
+      .replace(/<!--info-footer-start-->[\s\S]*<!--info-footer-end-->/, '')
+      .trim();
+  }
+
+  // Private function to load quiz metadata
+  function loadInfo(s: string): Partial<QuizMetaInfo> | undefined {
+    try {
+      const object = YAML.load(s) as any;
+      if (!object) {
+        return undefined;
+      }
+
+      const arrayKeys = ['tags', 'related'];
+
+      for (const key of arrayKeys) {
+        if (object[key]) {
+          object[key] = (object[key] || '')
+            .toString()
+            .split(',')
+            .map((i: string) => i.trim())
+            .filter(Boolean);
+        } else {
+          object[key] = undefined;
+        }
+      }
+
+      return object;
+    } catch (error) {
+      console.error(`Error while loading quiz metadata: ${error.message}`);
+      return undefined;
+    }
+  }
+
+  // Private function to load a quiz
+  async function loadQuiz(dir: string): Promise<Quiz> {
+    try {
+      return {
+        no: Number(dir.replace(/^(\d+)-.*/, '$1')),
+        difficulty: dir.replace(/^\d+-(.+?)-.*$/, '$1') as any,
+        path: dir,
+        info: await loadLocaleVariations(path.join(QUIZ_ROOT, dir, 'info.yml'), loadInfo),
+        readme: await loadLocaleVariations(path.join(QUIZ_ROOT, dir, 'README.md'), readmeCleanUp),
+        template: await loadFile(path.join(QUIZ_ROOT, dir, 'template.ts')) || '',
+        tests: await loadFile(path.join(QUIZ_ROOT, dir, 'test-cases.ts')),
+      };
+    } catch (error) {
+      console.error(`Error while loading quiz ${dir}: ${error.message}`);
+      throw error; // Rethrow the error to indicate failure
+    }
+  }
+
+  // Public function to load all quizzes
+  async function loadQuizes(): Promise<Quiz[]> {
+    try {
+      const folders = await fg('{0..9}*-*', {
+        onlyDirectories: true,
+        cwd: QUIZ_ROOT,
+      });
+
+      const quizes = await Promise.all(
+        folders.map(async (dir) => loadQuiz(dir)),
+      );
+
+      return quizes;
+    } catch (error) {
+      console.error(`Error while loading quizzes: ${error.message}`);
+      return [];
+    }
+  }
+
+  // Public function to load a quiz by number
+  async function loadQuizByNo(no: number | string): Promise<Quiz | undefined> {
+    try {
+      const folders = await fg(`${no}-*`, {
+        onlyDirectories: true,
+        cwd: QUIZ_ROOT,
+      });
+
+      if (folders.length) {
+        return await loadQuiz(folders[0]);
+      }
+
+      return undefined;
+    } catch (error) {
+      console.error(`Error while loading quiz by number ${no}: ${error.message}`);
+      return undefined;
+    }
+  }
+
+  // Public function to resolve quiz metadata for a specific locale
+  function resolveInfo(quiz: Quiz, locale: string = defaultLocale): QuizMetaInfo {
+    const info = Object.assign({}, quiz.info[defaultLocale], quiz.info[locale]);
+    info.tags = quiz.info[locale]?.tags || quiz.info[defaultLocale]?.tags || [];
+    info.related = quiz.info[locale]?.related || quiz.info[defaultLocale]?.related || [];
+
+    if (typeof info.tags === 'string') {
+      info.tags = info.tags.split(',').map((i) => i.trim()).filter(Boolean);
+    }
+
+    return info as QuizMetaInfo;
+  }
+
+  // Public interface
   return {
-    no: Number(dir.replace(/^(\d+)-.*/, '$1')),
-    difficulty: dir.replace(/^\d+-(.+?)-.*$/, '$1') as any,
-    path: dir,
-    info: await loadLocaleVariations(path.join(QUIZ_ROOT, dir, 'info.yml'), loadInfo),
-    readme: await loadLocaleVariations(path.join(QUIZ_ROOT, dir, 'README.md'), readmeCleanUp),
-    template: await loadFile(path.join(QUIZ_ROOT, dir, 'template.ts')) || '',
-    tests: await loadFile(path.join(QUIZ_ROOT, dir, 'test-cases.ts')),
-  }
-}
+    loadQuizes,
+    loadQuizByNo,
+    resolveInfo,
+  };
+})();
 
-export async function loadQuizByNo(no: number | string) {
-  const folders = await fg(`${no}-*`, {
-    onlyDirectories: true,
-    cwd: QUIZ_ROOT,
-  })
-
-  if (folders.length)
-    return await loadQuiz(folders[0])
-
-  return undefined
-}
-
-export function resolveInfo(quiz: Quiz, locale: string = defaultLocale) {
-  const info = Object.assign({}, quiz.info[defaultLocale], quiz.info[locale])
-  info.tags = quiz.info[locale]?.tags || quiz.info[defaultLocale]?.tags || []
-  info.related = quiz.info[locale]?.related || quiz.info[defaultLocale]?.related || []
-
-  if (typeof info.tags === 'string')
-    // @ts-expect-error
-    info.tags = info.tags.split(',').map(i => i.trim()).filter(Boolean)
-
-  return info as QuizMetaInfo
-}
+export default QuizLoader;
